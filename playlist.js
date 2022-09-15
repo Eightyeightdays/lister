e841f67faabf872926d81c463db337964aa0f069
Eightyeightdays <eightyeightdays@gmail.com>
2022-09-15 16:46:22 +0200

    current video can be added to playlist and a link to that playlist is generated on click

--- playlist.js
+++ playlist.js
@@ -2,22 +2,24 @@ let main = document.getElementById("main");
 main.style.background = "gold";
 main.style.borderRadius = "15px";
 main.style.padding = "10px";
-document.getElementById("content-text").style.color = "white !important";
-document.getElementById("upload-info").style.background = "dodgerblue";
-
-// let menu = document.querySelector("#top-level-buttons-computed");
-// menu.style.background = "gold";
+/////////////////
 
 let menuContainer = document.getElementById("menu-container")
 
 menuContainer.insertAdjacentHTML("afterbegin", 
     `<div id="playlist-interface">
         <button id="add-to-playlist">Add To Playlist</button>
+        <button id="create-link">Create Link</button>
+        <a href="" id="playlist-link">Link to your playlist</a>
     </div>`
 );
 
 let interface = document.getElementById("playlist-interface");
 let addButton = document.getElementById("add-to-playlist");
+let playlistLink = document.getElementById("playlist-link");
+let createButton = document.getElementById("create-link");
+createButton.addEventListener("click", createPlaylist);
+addButton.addEventListener("click", addToPlaylist);
 
 Object.assign(interface.style, {
     background: "dodgerblue",
@@ -38,13 +40,22 @@ Object.assign(addButton.style, {
 let playlistString = "";
 let baseUrl = "https://www.youtube.com/watch_videos?video_ids=";
 
-function getVideoId(){
-    let currentUrl = window.location.href;
-}
 
 function addToPlaylist(){
-    
+    let currentUrl = window.location.href;
+    let start = currentUrl.search(/=/) + 1;
+    let end = start + 12;
+    let currentId = currentUrl.substring(start, end) + ",";
+   
+    playlistString += currentId;
+    localStorage.setItem("playlistString", playlistString);
+    console.log("STRING: " + localStorage.getItem("playlistString"));
+}
 
+function createPlaylist(){
+    let videoList = localStorage.getItem("playlistString");
+    let fullUrl = baseUrl + videoList;
+    playlistLink.href = fullUrl;
 }
 
 
