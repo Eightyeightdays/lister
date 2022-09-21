fee1429f225faedff472d168594b69ce26cfc215
Eightyeightdays <eightyeightdays@gmail.com>
2022-09-21 13:30:56 +0200

    functions clarified, ui improved, localStorage schema updated

--- content_scripts/makePlaylist.js
+++ content_scripts/makePlaylist.js
@@ -6,6 +6,42 @@
     window.hasRun = true;
     ////////////////////
 
+    // check if localStorage is available on the browser // ^^ Maybe move inside the if statement
+    function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);  
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+        } 
+    }
+    
+    if(storageAvailable('localStorage')) {
+        console.log("localStorage is available on this browser");
+    }
+    else{
+        console.log("Attention, localStorage is not available on this browser!");
+        return; // EXIT IF UNAVAILABLE
+    } 
+
+
+
     let playlistString = "";
     let baseUrl = "https://www.youtube.com/watch_videos?video_ids=";
     // let playlistLink = document.getElementById("playlist-link");
@@ -22,29 +58,44 @@
     function setPlaylistName(name){
         let newList = {
             playlistName: name,
-            playlistStrings: [],
+            videos: [],
+            playlistString: "",
             dateCreated: Date.now(),
             dateEdited: "",
             lastAccessed: ""
             }
 
-        if(!localStorage.getItem("allPlaylists")){  // if no playlist exists
+        if(!localStorage.getItem("allPlaylists")){                              // if no playlist exists
             localStorage.setItem("allPlaylists", JSON.stringify([newList]));    // initialise localStorage with allPlaylists
         }else{
             let tempStorage = JSON.parse(localStorage.getItem("allPlaylists")); // otherwise get existing storage
-            tempStorage.push(newList);      // add new list
+            tempStorage.push(newList);                                          // add new list
             localStorage.setItem("allPlaylists", JSON.stringify(tempStorage))   // update localStorage
         }
-        
-        
     }
 
-    function updateLocalStorage(){
+    function updateCurrentList(data){
+        let tempData = JSON.parse(localStorage.getItem("allPlaylists"));
+        
         let currentUrl = window.location.href;
         let start = currentUrl.search(/=/) + 1;
         let end = start + 12;
         let currentId = currentUrl.substring(start, end) + ",";
-        playlistString += currentId;
+
+        // get currently selected list
+        // get playListString of current list
+        playlistString += currentId;    // need to get old string from storage and add to it
+        
+        
+        
+
+        let newVideo = {
+            id: currentId,
+            title: data.title,
+            author: data.author,
+            imgUrl: data.imgUrl
+        }
+
         localStorage.setItem("allPlaylists", playlistString);
     }
 
@@ -64,74 +115,33 @@
         return videoDetails;
     }
    
-    browser.runtime.onMessage.addListener(message => {  // listen for messages from the popup
-        console.log(JSON.stringify(message))
+// Message handler //
+
+    function handleCommands(message){
+        console.log(message.command)
 
         if(message.command === "add name"){
             setPlaylistName(message.title)
         }else if(message.command === "add video") {
             return getVideoDetails()    // adding "return" here solved the problem
-            .then(updateLocalStorage())
+            .then(updateCurrentList(details))
             .then(details =>{
                 return Promise.resolve({message: "video details fetched", details: details})
             })
             .catch(error => console.log(error))
         }else if(message.command === "create link"){
             createPlaylistLink();
+        }else if(message.command === "return localStorage"){
+            if(localStorage.getItem("allPlaylists")){
+                return Promise.resolve({message: "Storage retrieved", storage: JSON.parse(localStorage.getItem("allPlaylists"))})
+            }else{
+                return Promise.reject({message: "Storage is empty"})
+            } 
         }else{
             return;
-            // console.log("UNKNOWN MESSAGE")
-        }
-
-    })
-
-
-    // check if localStorage is available on the browser
-    function storageAvailable(type) {
-        let storage;
-        try {
-            storage = window[type];
-            const x = '__storage_test__';
-            storage.setItem(x, x);  
-            storage.removeItem(x);
-            return true;
-        }
-        catch (e) {
-            return e instanceof DOMException && (
-                // everything except Firefox
-                e.code === 22 ||
-                // Firefox
-                e.code === 1014 ||
-                // test name field too, because code might not be present
-                // everything except Firefox
-                e.name === 'QuotaExceededError' ||
-                // Firefox
-                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-                // acknowledge QuotaExceededError only if there's something already stored
-                (storage && storage.length !== 0);
-        }
-        
-    }
-    
-    if(storageAvailable('localStorage')) {
-        console.log("localStorage is available on this browser");
-    }
-    else{
-        console.log("Attention, localStorage is not available on this browser!");
-    } 
-    
-
-    function returnStorage(message){
-        if(message.message === "return localStorage"){
-            console.log(message.message)
-           if(localStorage.getItem("allPlaylists")){
-            browser.runtime.sendMessage({message: "Storage retrieved", storage: localStorage.getItem("allPlaylists")})
-            }else{
-                browser.runtime.sendMessage({message: "Storage is empty"})
-            } 
         }
     }
 
-    browser.runtime.onMessage.addListener(returnStorage)
+    browser.runtime.onMessage.addListener(handleCommands);
 
 })()
--- popup/playlist.js
+++ popup/playlist.js
@@ -2,25 +2,10 @@ var videoNumber = 1;
 
 function listenForClicks() {
     document.addEventListener("click", (e) => {
-   
-        function createVideoCard(info){
-            let playlistPreview = document.getElementById("playlist-preview");
-
-            let card = `
-            <div class="video-card" id="playlist-video-${videoNumber}">
-                <div class="playlist-video-title">${info.title}</div>
-                <img class="playlist-preview-image" src=${info.imgUrl} alt="${info.title}">
-                <div class="playlist-video-author">Uploaded by: ${info.author}</div>
-            </div>`;
-
-            playlistPreview.insertAdjacentHTML("afterbegin", card); // insert video card
-
-            videoNumber++;  // increment id number
-        }
 
         function addName(tabs) {
             let title = document.getElementById("playlist-name-input").value;
-            let element = `<div class="list-title">${title}</div>`;
+            let element = `<div class="list-title" id=${title}>${title}</div>`;
             browser.tabs.sendMessage(tabs[0].id, {
                 command: "add name",
                 title: title
@@ -50,7 +35,7 @@ function listenForClicks() {
                 console.log(response) // empty for now
             })
         }
-
+    
         if(e.target.id === "add-playlist-name"){
             browser.tabs.query({active: true, currentWindow: true})
                 .then(addName)
@@ -60,49 +45,60 @@ function listenForClicks() {
         }else if(e.target.id === "create-link"){
             browser.tabs.query({active: true, currentWindow: true})
                 .then(createLink)
-        }else if(e.target.id === ""){
-
+        }else if(e.target.classList.contains("list-title")){
+            selectPlaylistTitle(e.target.id)
+            document.getElementById("current-playlist").textContent = e.target.id;
         }
 
     })
 }
 
-
-
-
-function getInitialStorage(){   // runs every time popup is opened
-    browser.tabs.query({active: true, currentWindow: true})
-    .then(response => browser.tabs.sendMessage(response[0].id, {message: "return localStorage"}))
+function createVideoCard(info){
+    let playlistPreview = document.getElementById("playlist-preview");
+    let card = `
+    <div class="video-card" id="playlist-video-${videoNumber}">
+        <div class="playlist-video-title">${info.title}</div>
+        <img class="playlist-preview-image" src=${info.imgUrl} alt="${info.title}">
+        <div class="playlist-video-author">Uploaded by: ${info.author}</div>
+    </div>`;
+    playlistPreview.insertAdjacentHTML("afterbegin", card); // insert video card
+    videoNumber++;  // increment id number
 }
 
-function initialiseUi(data){
+function getLocalStorage(){   // runs every time popup is opened    // localStorage is returned here
+    browser.tabs.query({active: true, currentWindow: true})
+    .then(response => browser.tabs.sendMessage(response[0].id, {command: "return localStorage"})) // send message
+    .then(response => createTitlesList(response.storage)) 
+}
 
+function selectPlaylistTitle(id){
+    let playlistName = document.getElementById(id).textContent;
+    getSelectedTitleData(playlistName)
+}
+
+function getSelectedTitleData(playlistName){
+    browser.tabs.query({active: true, currentWindow: true})
+    .then(response => browser.tabs.sendMessage(response[0].id, {command: "return localStorage"})) // get storage
+    .then(response=>{
+       response.storage.forEach(list =>{
+            if(list["playlistName"] === playlistName){  // find the matching playlist
+                createVideoCard(list)
+                console.log("CREATED")
+            }   
+        }) 
+    })
 }
 
 function createTitlesList(data){
+    console.log(`DATA PASSED TO createTitlesList: ${data}`)
+
     data.forEach(list => {
-        let title = `<div class="list-title">${list.playlistName}</div>`;
+        let title = `<div class="list-title" id=${list.playlistName}>${list.playlistName}</div>`;
         document.getElementById("list-title-container").insertAdjacentHTML("afterbegin", title)
     });
 }
 
-browser.runtime.onMessage.addListener(message =>{
-    if(message.message === "Storage retrieved"){
-        console.log("Storage retrieved, UI BEING REBUILT")
-        createTitlesList(JSON.parse(message.storage))
-    }else if(message.message === "Storage is empty"){
-        console.log("Nothing in storage");
-        /// PROMPT USER TO CREATE A LIST
-        return;
-    }
-    
-})
-
-browser.runtime.onConnect.addListener(getInitialStorage)
-
 browser.tabs.executeScript({file: "/content_scripts/makePlaylist.js"})
-    .then(getInitialStorage)
-    .then(console.log("EXECUTED"))
+    .then(getLocalStorage)
     .then(listenForClicks)
     .catch();
-
