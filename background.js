d8836f014200183427d26a317b6f4fa0d35280a6
Eightyeightdays <eightyeightdays@gmail.com>
2022-12-30 13:47:02 +0100

    Icon created and added to context menu

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function onCreated() {
     if (browser.runtime.lastError) {
         console.log(`Error: ${browser.runtime.lastError}`);
     } else {
-        console.log("Context menu item created successfully");
+        console.log("Context menu item created successfully!");
     }
 }
 
@@ -10,25 +10,28 @@ browser.contextMenus.create({
     id: "add",
     title: "Add To Playlist",
     contexts: ["all"],
-    documentUrlPatterns: ["*://www.youtube.com/*"]
+    documentUrlPatterns: ["*://www.youtube.com/*"],
+    icons: {
+        "16": "icons/Lister-16.png",
+        "32": "icons/Lister-32.png"
+    }
 }, onCreated);
 
 browser.contextMenus.onClicked.addListener((info) => {
     if(info.menuItemId === "add"){
         if(info.linkUrl === undefined){
-                console.log("An error occurred trying to add the video URL")
+            console.log("An error occurred trying to add the video URL")
+            return;
         }else{
-                browser.tabs.query({active: true, currentWindow: true})
-                .then(response =>{
-                    browser.tabs.sendMessage(response[0].id, {
-                        command: "add url", 
-                        url: info.linkUrl
-                    })
-                    .then(response => console.log(response))
-                    .catch(error => console.log(error))
+            browser.tabs.query({active: true, currentWindow: true})
+            .then(response =>{
+                browser.tabs.sendMessage(response[0].id, {
+                    command: "add url", 
+                    url: info.linkUrl
                 })
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
+            })
         }
-        
-        return;
     }
 })
